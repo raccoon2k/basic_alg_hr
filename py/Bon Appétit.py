dae964a294d37f0cbbef2ce0e1d8a2d87c977e3f
raccoon_2k <banhmy1290@gmail.com>
2020-07-27 01:51:49 +0700

    add python 3

---
+++ py/Bon Appétit.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+prices = list(map(int, input().split()))
+balance = int(input())
+total = int((sum(prices) - prices[k]) / 2)
+if balance == total:
+    print('Bon Appetit') 
+else:
+    print(balance-total)
